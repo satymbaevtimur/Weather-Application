97d61ea93cf9f0d8b3d05b3c814af9a95dd2d060
Timur <140198814+satymbaevtimur@users.noreply.github.com>
2023-12-16 13:15:15 +0000

    Update

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import requests
 
 app = Flask(__name__)
+weather_api_token = '800b1154a23f431d947180647231512'
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route("/", methods=['GET'])
+def index():
+    city = request.args.get('search', '')
+    
+    if city != '':
+        weather_data = get_weather(city)
+    else:
+        weather_data = get_weather()
+    
+    return render_template("index.html", weather_data=weather_data)
+
+def get_weather(city_name=''):
+    if city_name != '':
+        weather_api_url = f"http://api.weatherapi.com/v1/current.json?key={weather_api_token}&q={city_name}"
+    else:
+        weather_api_url = f"http://api.weatherapi.com/v1/current.json?key={weather_api_token}&q=London"
+    response = requests.get(weather_api_url)
+
+
+    return response.json()
+
+@app.route('/test')
+def test():
+    weather_api_url = f"http://api.weatherapi.com/v1/current.json?key={weather_api_token}&q=Astana"
+    response = requests.get(weather_api_url)
+
+    return response.json()
